cf2454102f1ed270fa0cc9b35a8f2b6f0f060e7a
Vadim isametov <isametoff@gmail.com>
2022-10-19 17:54:41 +0200

    Тестовые данные для БД, Laravel Seeding

---
+++ database/factories/ProductsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cart>
+ */
+class ProductsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->unique()->company,
+            'description' => $this->faker->paragraph,
+            'content' => $this->faker->title(),
+            'price' => $this->faker->numberBetween(1500, 6000),
+            'rest' => $this->faker->numberBetween(1, 6),
+            'is_published' => 1,
+        ];
+    }
+}
